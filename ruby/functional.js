a61f0451a8fa54a022ca3bab960f4054dfe972d6
MalachiCoberley <malachicoberley@gmail.com>
2022-02-25 11:43:01 -0700

    cycle link list challenge

--- ruby/functional.js
+++ ruby/functional.js
@@ -19,3 +19,27 @@ var sortList = function(head) {
     }
     return head
 };
+
+
+
+// linked list cycle
+var hasCycle = function(head) {
+  if (!head || !head.next) {
+      return false
+  }
+  let cycled = false;
+  let currentNode = head;
+  let i = 0;
+  const mapper = new Map()
+  while (!cycled && currentNode.next) {
+      if (mapper.get(currentNode)) {
+          cycled = true
+      } else {
+          mapper.set(currentNode, i)
+      }
+      i++
+      currentNode = currentNode.next
+  }
+      
+  return cycled
+};
